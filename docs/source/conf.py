9944ba76d3019563f44353639e07a465e234f1c4
Jon Duckworth <duckontheweb@gmail.com>
2020-12-22 11:35:07 -0500

    Include source code in API docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,6 +42,7 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx_rtd_theme',
     'sphinx.ext.autosectionlabel',
+    'sphinx.ext.viewcode'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
