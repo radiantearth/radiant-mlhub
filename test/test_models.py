5e0955869b51a2a1610db2bfe3c6511cd52ef086
Jon Duckworth <duckontheweb@gmail.com>
2021-06-03 17:21:10 -0400

    Add failing test

--- test/test_models.py
+++ test/test_models.py
@@ -1,7 +1,9 @@
 import pystac
 import pytest
+from urllib.parse import urljoin
 
 from radiant_mlhub.models import Collection, Dataset
+from radiant_mlhub.session import Session
 
 
 class TestCollection:
@@ -116,3 +118,79 @@ class TestDataset:
     def test_total_archive_size(self):
         dataset = Dataset.fetch('bigearthnet_v1')
         assert dataset.total_archive_size == 71311240007
+
+
+class TestDatasetNoProfile:
+    DATASET = {
+        "citation": "Fake citation",
+        "collections": [
+            {
+                "id": "test_collection",
+                "types": [
+                    "source_imagery"
+                ]
+            }
+        ],
+        "doi": "10.12345/depositonce-12345",
+        "id": "test_dataset",
+        "registry": "https://registry.mlhub.earth/10.12345/depositonce-12345",
+        "title": "Test Dataset"
+    }
+
+    COLLECTION = {
+        "description": "Test Collection",
+        "extent": {
+            "spatial": {
+                "bbox": [
+                    [
+                        -9.00023345437725, 1.7542686833884724,
+                        83.44558248555553,68.02168200047284
+                    ]
+                ]
+            },
+            "temporal": {
+                "interval": [
+                    [
+                        "2017-06-13T10:10:31Z",
+                        "2018-05-29T11:54:01Z"
+                    ]
+                ]
+            }
+        },
+        "id": "test_collection",
+        "links": [],
+        "license": "Test License",
+        "properties": {},
+        "stac_version": "1.0.0-beta.2"
+    }
+    @pytest.fixture(scope='function', autouse=True)
+    def mock_profile(self, monkeypatch, tmp_path):
+        """Overwrite the fixture in conftest so we don't set up an API key here"""
+
+        # Monkeypatch the user's home directory to be the temp directory
+        # This prevents the client from automatically finding any profiles configured in the user's
+        # home directory.
+        monkeypatch.setenv('HOME', str(tmp_path))  # Linux/Unix
+        monkeypatch.setenv('USERPROFILE', str(tmp_path))  # Windows
+
+        yield
+
+    def test_get_collections_with_api_key(self, requests_mock):
+        """The Dataset class should use any API keys passed during instantiation for listing
+        collections."""
+        dataset_id = self.DATASET["id"]
+        collection_id = self.COLLECTION["id"]
+        api_key = 'test_api_key'
+
+        dataset_url = urljoin(Session.ROOT_URL, f'datasets/{dataset_id}')
+        requests_mock.get(dataset_url, json=self.DATASET)
+
+        collection_url = urljoin(Session.ROOT_URL, f'collections/{collection_id}')
+        requests_mock.get(collection_url, json=self.COLLECTION)
+
+        dataset = Dataset.fetch(dataset_id, api_key=api_key)
+        _ = dataset.collections
+
+        history = requests_mock.request_history
+        assert len(history) == 2
+        assert "key=test_api_key" in history[1].url
